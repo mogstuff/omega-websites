6a139924f403504043917ba4f00ae6a36186327f
Mark Morgan <markmorgan.mcp@gmail.com>
2021-04-07 16:34:07 +0100

    feat: build links dynamically
    testing dynamic building of links in top nav

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
+const topNavList = document.getElementById('top-nav-list');
 const sections = document.querySelectorAll('.content');
 const navLinks = [];
 
 const buildTopNavMenu = (sections) => {    
 
   for(let s of sections){
-     console.log(s.dataset['title']);     
+     let link = `<li><a href="#${s.id}">${s.dataset['title']}</a></li>`;     
+     console.log(link);
+     navLinks.push(link);
   }
+
+  topNavList.innerHTML = navLinks;
 
  }  
    
