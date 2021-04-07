a76a1567399e1eb4ec10492850750b85bcd6493d
Mark Morgan <markmorgan.mcp@gmail.com>
2021-04-07 16:16:33 +0100

    feat: build nav menu function
    first pass at building top nav menu dynamically. Employs data attribute on each section block

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 const sections = document.querySelectorAll('.content');
 const navLinks = [];
 
+const buildTopNavMenu = (sections) => {    
+
+  for(let s of sections){
+     console.log(s.dataset['title']);     
+  }
+
+ }  
+   
+
+buildTopNavMenu(sections);
